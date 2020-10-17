c50244a7b56d690877e2a44a2bd878841589aead
Le Boucher Alexis <dev.alexisleboucher@gmail.com>
2020-10-17 13:37:48 +0200

    Send a 404 if route doesn't exist + comments

--- server/index.js
+++ server/index.js
@@ -7,9 +7,13 @@ app.use(bodyParser.json());
 
 app.use(express.static('dist'));
 
+// Send the username of the os user
 app.get('/api/getUsername', (req, res) => {
   res.send({ username: os.userInfo().username })
 });
+
+// Send a 404 error if request starts by '/api/' but route doesn't exist
+app.use('/api/*', (req, res) => { throw createError(404, "Page Not Found") });
 
 // Always send index.html for the requests not starting by '/api' 
 app.use((req, res) => { 
